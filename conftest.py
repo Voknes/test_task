b3e549e62d3e241890c384c341954b99a2ce5c4a
Voknes <redvoknes@yandex.ru>
2022-08-25 04:35:01 +0700

    Edit conftest.py

--- conftest.py
+++ conftest.py
@@ -1,6 +1,5 @@
 import pytest
 from selenium import webdriver
-# from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
